77e095819e18567c12c97601b6276e9a41a1fea8
w-sr <worldsr.up@gmail.com>
2023-04-07 11:30:51 -0400

    Update env

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -2,4 +2,6 @@ import { TaxBracket } from "../models/tax";
 import { axg } from "./axios";
 
 export const fetchTaxBrackets = async (year: number): Promise<TaxBracket[]> =>
-  axg(`${process.env["E2E_BASE_URL"]}/${year}`);
+  axg(
+    `${process.env["REACT_APP_API_ENDPOINT"]}/tax-calculator/tax-year/${year}`
+  );
